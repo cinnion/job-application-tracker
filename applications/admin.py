e99f428a7ac4c3d3538517f6f8376ebde86e5236
Douglas Needham <cinnion@gmail.com>
2026-03-18 17:58:10 -0400

    Add django-upgrade and run it for a 6.0 target
    - Add the django-upgrade package.
    - Switch to using the admin.register decorator on JobApplicationAdmin.
    - DEFAULT_AUTO_FIELD is unneeded since BigAutoField is now the default.

--- applications/admin.py
+++ applications/admin.py
@@ -2,11 +2,9 @@ from django.contrib import admin
 from .models import JobApplication
 
 
+@admin.register(JobApplication)
 class JobApplicationAdmin(admin.ModelAdmin):
     """
     Model for doing administration of JobApplications
     """
     readonly_fields = ('created_at', 'updated_at')
-
-
-admin.site.register(JobApplication, JobApplicationAdmin)
--- unemployment/settings.py
+++ unemployment/settings.py
@@ -186,7 +186,6 @@ WEBPACK_LOADER = {
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Login/Logout related settings.
 LOGIN_REDIRECT_URL = '/'
