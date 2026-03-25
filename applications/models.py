61be1507663f4c18f5999733b1a568a03dcfea8b
Douglas Needham <cinnion@gmail.com>
2026-03-25 17:45:42 -0400

    Reformat applications/models.py
    - Reformat applications/models.py, to split each field into a multi-line format
      with some consistency on argument order.

--- applications/models.py
+++ applications/models.py
@@ -3,16 +3,51 @@ from django.utils.timezone import localtime
 
 
 class JobApplication(models.Model):
-    when = models.DateField(default=localtime, blank=True, db_comment="Date of application.")
-    company = models.CharField(max_length=64, db_comment="Name of company.")
-    title = models.CharField(max_length=128, blank=True, db_comment="Job title.")
-    posting = models.URLField(blank=True, db_comment="URL of job posting, if applicable.")
-    confirm = models.URLField(blank=True, db_comment="URL for application confirmation, if applicable.")
-    notes = models.CharField(max_length=256, blank=True, db_comment="Notes")
-    active = models.BooleanField(default=True, db_comment="Application is still outstanding.")
-    rejected = models.DateField(blank=True, null=True, db_comment="Date of rejection notice.")
-    created_at = models.DateTimeField(auto_now_add=True, db_comment="Record created at.", null=True)
-    updated_at = models.DateTimeField(auto_now=True, db_comment="Record last updated at.")
+    when = models.DateField(
+        default=localtime,
+        blank=True,
+        db_comment="Date of application."
+    )
+    company = models.CharField(
+        max_length=64,
+        db_comment="Name of company."
+    )
+    title = models.CharField(
+        max_length=128,
+        blank=True,
+        db_comment="Job title."
+    )
+    posting = models.URLField(
+        blank=True,
+        db_comment="URL of job posting, if applicable."
+    )
+    confirm = models.URLField(
+        blank=True,
+        db_comment="URL for application confirmation, if applicable."
+    )
+    notes = models.CharField(
+        max_length=256,
+        blank=True,
+        db_comment="Notes"
+    )
+    active = models.BooleanField(
+        default=True,
+        db_comment="Application is still outstanding."
+    )
+    rejected = models.DateField(
+        blank=True,
+        null=True,
+        db_comment="Date of rejection notice."
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        null=True,
+        db_comment="Record created at.",
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        db_comment="Record last updated at."
+    )
 
     class Meta:
         db_table = 'job_applications'
