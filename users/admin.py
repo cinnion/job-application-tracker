6d912234c856d86ebe4aec55e42b8fdd9a18a101
Douglas Needham <cinnion@gmail.com>
2026-03-28 21:42:44 -0400

    Register our extended user app with the Django admin interface
    - Update users.admin to create our own UserAdmin class extending the
      django.contrib.auth.admin.UserAdmin class, with no changes to the admin
      model for now.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(AuthUserAdmin):
+    pass
