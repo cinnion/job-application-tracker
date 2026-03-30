12696873f9943b238616f01b8ab7906126778bfa
Douglas Needham <cinnion@gmail.com>
2026-03-30 19:19:19 -0400

    Update the JobApplication model
    - Add a verbose name to each field, with translation which can be added later.
    - Add help text to each field, with translation which can be added later.
    - Explicitly add our primary key.
    - Add the foreign key to the User model, with CASCADE ON DELETE.
      We call this user instead of user_id, since we will be setting this field
      to a User model instance, and the framework will be taking the id from that
      to turn it into a user_id field.
    - Change the company to make it required by not specifying blank=True.
      We have 1 early records where this was the case, and it has been updated.
    - Change the title to make it required. If we are applying for a job for which
      we do not know the title, we should note this.
    - Allow the posting URL to be blank/NULL. Not all jobs are applied to via a
      posting on a job board.
    - Allow confirm to be blank/NULL. We have mostly given up tracking the
      confirmation emails, since Gmail makes it difficult to permalink an email, and
      not all mail systems allow this.
    - Update notes to become a TextField, and allow it to be blank/NULL. This will
      mean that the resulting textarea can be roughly 80*51, giving us sufficient
      space for recording things like salary expectations, interview dates, and
      more.
    - Add an interviews field, which will be a postive integer up to 32767.
    - Add an interfiewed property, which will return True if interviews is non-zero,
      else it will return False.
    - Update the __str__ dunder, to use an f-string.
    - Regenerate the migration for the job_application module.

--- applications/models.py
+++ applications/models.py
@@ -1,56 +1,140 @@
 from django.db import models
 from django.utils.timezone import localtime
+from django.utils.translation import gettext_lazy as _
+from django.conf import settings
 
 
 class JobApplication(models.Model):
+    """
+    A database record for storing information about job applications.
+    """
+    class Meta:
+        db_table = "job_applications"
+
+    id = models.BigAutoField(
+        _("id"),
+        primary_key=True,
+        help_text=_(
+            "The primary key for the job application"
+        ),
+        db_comment="Primary key",
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name=_("user id"),
+        related_name="job_applications",
+        db_comment="The user who submitted the application."
+    )
     when = models.DateField(
+        _("when"),
         default=localtime,
         blank=True,
+        help_text=_(
+            "The date when the application was submitted"
+        ),
         db_comment="Date of application."
     )
     company = models.CharField(
+        _("company name"),
         max_length=64,
+        help_text=_(
+            "The name of the company."
+        ),
         db_comment="Name of company."
     )
     title = models.CharField(
+        _("job title"),
         max_length=128,
-        blank=True,
+        help_text=_(
+            "The job title."
+        ),
         db_comment="Job title."
     )
     posting = models.URLField(
+        _("job posting url"),
         blank=True,
+        null=True,
+        help_text=_(
+            "The URL for the job posting, if applicable. Use notes for other job sources"
+        ),
         db_comment="URL of job posting, if applicable."
     )
     confirm = models.URLField(
+        _("confirmation email url"),
         blank=True,
+        null=True,
+        help_text=_(
+            "The URL of the confirmation email"
+        ),
         db_comment="URL for application confirmation, if applicable."
     )
-    notes = models.CharField(
-        max_length=256,
+    notes = models.TextField(
+        _("notes"),
+        max_length=4096,
         blank=True,
+        null=True,
+        help_text=_(
+            "Notes about this job, such as alternate sources, expected salary, etc."
+        ),
         db_comment="Notes"
     )
     active = models.BooleanField(
+        _("application still active"),
         default=True,
+        help_text=_(
+            "True if the job application is still potentially active"
+        ),
         db_comment="Application is still outstanding."
     )
+    interviews = models.PositiveSmallIntegerField(
+        _("interviews"),
+        default=0,
+        help_text=_(
+            "The number of interviews. More details like dates can go in the notes field."
+        ),
+        db_comment="The number of interviews for the job."
+    )
     rejected = models.DateField(
+        _("rejection date"),
         blank=True,
         null=True,
+        help_text=(
+            "When a job rejection letter was received"
+        ),
         db_comment="Date of rejection notice."
     )
     created_at = models.DateTimeField(
+        _("created_at"),
         auto_now_add=True,
         null=True,
+        help_text=_(
+            "When the record was created"
+        ),
         db_comment="Record created at.",
     )
     updated_at = models.DateTimeField(
+        _("updated_at"),
         auto_now=True,
+        help_text=_(
+            "When the record was last updated",
+        ),
         db_comment="Record last updated at."
     )
 
-    class Meta:
-        db_table = 'job_applications'
+    @property
+    def interviewed(self) -> bool:
+        """
+        Whether we have interviewed for the job.
+        """
+        if self.interviews > 0:
+            return True
+
+        return False
 
     def __str__(self):
-        return '{}: {} @ {}'.format(self.when, self.title, self.company)
+        """
+        Method used when converting to a string. Only return a far simpler representation
+        of the record in this instance.
+        """
+        return f"{self.when}: {self.title} @ {self.company}".format(self.when, self.title, self.company)
