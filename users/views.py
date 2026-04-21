43259e36541910e16124ec2b08d64431ee40a79d
Douglas Needham <douglas.w.needham@gmail.com>
2026-04-20 21:38:44 -0400

    Update some of our user views.
    - Bring over the function email_verification_sent(), so that it can find
      the correct view classes.
    - Create our custom ConfirmEmailView which extends the one from allauth but
      uses our template.
    - Create our custom ConfirmEmailVerificationCodeView which extends the one
      from allauth but uses our template.
    - Create our custom EmailVerificationSentView which extends the one from allauth
      but uses our template.
    - Create our custom LoginView which extends the one from allauth but which uses
      our template.
    - Update PasswordChangeView to extend the one from allauth instead of
      django.contrib.auth.

--- users/views.py
+++ users/views.py
@@ -1,21 +1,59 @@
 """
 Our overridden user views.
 """
-from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
-from django.contrib.auth.views import (
-    PasswordChangeView as AuthPasswordChangeView
+from allauth.account.views import (
+    ConfirmEmailView as AllauthConfirmEmailView,
+    ConfirmEmailVerificationCodeView as AllauthConfirmEmailVerificationCodeView,
+    EmailVerificationSentView as AllauthEmailVerificationSentView,
+    LoginView as AllauthLoginView,
+    PasswordChangeView as AllauthPasswordChangeView,
 )
+from django.utils.decorators import method_decorator
+from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import UpdateView
+from allauth.account.internal.decorators import login_not_required
+from django.http import (
+    Http404,
+    HttpRequest,
+    HttpResponse,
+    HttpResponseBase,
+    HttpResponseRedirect,
+)
+from allauth.account import app_settings
 
 from core.views.mixins import Log404Mixin
 from .forms import ProfileForm
 from .models import User
 
 
-class PasswordChangeView(PermissionRequiredMixin, AuthPasswordChangeView):
+@method_decorator(login_not_required, name="dispatch")
+def email_verification_sent(request: HttpRequest) -> HttpResponseBase:
+    if app_settings.EMAIL_VERIFICATION_BY_CODE_ENABLED:
+        return ConfirmEmailVerificationCodeView.as_view()(request)
+    else:
+        return EmailVerificationSentView.as_view()(request)
+
+
+class ConfirmEmailView(AllauthConfirmEmailView):
+    template_name = "email_confirm.html"
+
+
+class ConfirmEmailVerificationCodeView(AllauthConfirmEmailVerificationCodeView):
+    template_name = "confirm_email_verification_code.html"
+
+
+class EmailVerificationSentView(AllauthEmailVerificationSentView):
+    template_name = "verification_sent.html"
+
+
+class LoginView(AllauthLoginView):
+    template_name = 'registration/login.html'
+
+
+class PasswordChangeView(PermissionRequiredMixin, AllauthPasswordChangeView):
     """
-    Extend the PasswordChangeView from django.contrib.auth to require the change_user_password permission
+    Extend the PasswordChangeView from allauth.account.views to require the change_user_password permission
     from our extended model.
     """
     http_method_names = ["get", "post"]
