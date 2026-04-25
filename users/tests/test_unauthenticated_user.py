b95d881098507312be2c575cfacb27995ab05db9
Douglas Needham <douglas.w.needham@gmail.com>
2026-04-24 23:30:54 -0400

    Add additional unauthenticated user tests for LoginView.
    - Use the LOGIN_URL from settings for the login_url class attribute.
    - Verify that the view extends user-master.html, and has the login and
      password fields.
    - Add test to make sure that an attempt to login with an unverified user sends
      the verification email.
    - Add test to make sure that a verified user can log in and gets redirected to
      the home page.

--- users/tests/test_unauthenticated_user.py
+++ users/tests/test_unauthenticated_user.py
@@ -5,17 +5,25 @@ actions.
 import unittest
 from urllib.parse import urlencode
 
+from allauth.account.models import EmailAddress
 from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.contrib.messages import (
+    Message,
+    constants as messages,
+)
+from django.contrib.messages.test import MessagesTestMixin
+from django.core import mail
 from django.urls import reverse
 
 from core.test import TestCase
 
 
-class TestUnauthenticatedLoginView(TestCase):
+class TestUnauthenticatedLoginView(MessagesTestMixin, TestCase):
     """
     This class tests our login view.
     """
-    login_url = reverse("account_login")
+    login_url = reverse(settings.LOGIN_URL)
 
     def test_get_receives_login_view(self):
         # Arrange
@@ -26,7 +34,68 @@ class TestUnauthenticatedLoginView(TestCase):
         # Assert
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "master.html")
+        self.assertTemplateUsed(response, "user-master.html")
         self.assertTemplateUsed(response, "registration/login.html")
+        form = response.context_data["form"]
+        self.assertEqual(form.fields['login'].label, "Username")
+        self.assertEqual(form.fields['password'].label, "Password")
+
+    def test_post_unverified_user_with_correct_credentials_gets_verification_page(self):
+        # Arrange
+        client = self.client_class()
+        username = "testuser1"
+        password = "Test user1 "
+        email = "test@example.com"
+        user = get_user_model().objects.create(username=username, email=email)
+        user.set_password(password)
+        user.save()
+
+        data = {
+            "login": user.username,
+            "password": password
+        }
+
+        # Act
+        response = client.post(self.login_url, data)
+
+        # Assert
+        self.assertRedirects(response,
+                             reverse("account_email_verification_sent"),
+                             )
+        self.assertMessages(response, [Message(messages.INFO, f"Confirmation email sent to {user.email}.")])
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].subject, "[testserver] Please Confirm Your Email Address")
+        self.assertEqual(mail.outbox[0].to, [user.email])
+
+    def test_post_verified_user_with_correct_credentials_gets_redirect_with_message(self):
+        # Arrange
+        client = self.client_class()
+        username = "testuser1"
+        password = "Test user1 "
+        email = "test@example.com"
+        user = get_user_model().objects.create(username=username, email=email)
+        user.set_password(password)
+        user.save()
+        EmailAddress.objects.create(
+            user=user,
+            email=user.email,
+            primary=True,
+            verified=True
+        )
+
+        data = {
+            "login": user.username,
+            "password": password
+        }
+
+        # Act
+        response = client.post(self.login_url, data)
+
+        # Assert
+        self.assertRedirects(response,
+                             reverse(settings.LOGIN_REDIRECT_URL),
+                             )
+        self.assertMessages(response, [Message(messages.SUCCESS, f"Successfully signed in as {user.username}.")])
 
 
 # noinspection DuplicatedCode - These tests are near duplicates of one another, but test different endpoints.
