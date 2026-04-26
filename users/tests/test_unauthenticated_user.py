5e76cbe9c2a54e627a8901e386de1950d6e4858c
Douglas Needham <douglas.w.needham@gmail.com>
2026-04-25 21:47:20 -0400

    Place SignupView under test to make sure it meets our requirements.
    - While tested as a part of allauth, test the SignupView to make sure it meets
      our requirements on things like the view templates rendered and password
      verification rules.

--- users/tests/test_unauthenticated_user.py
+++ users/tests/test_unauthenticated_user.py
@@ -19,6 +19,252 @@ from django.urls import reverse
 from core.test import TestCase
 
 
+class TestUnauthenticatedSignupView(MessagesTestMixin, TestCase):
+    """
+    Test the SignupView as an unauthenticated user.
+    """
+    signup_url = reverse("account_signup")
+    redirect_url = reverse("account_email_verification_sent")
+
+    def test_get_gets_form_with_correct_templates_and_fields_including_honeypot(self):
+        # Arrange
+
+        # Act
+        response = self.client.get(self.signup_url)
+
+        # Assert
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "master.html")
+        self.assertTemplateUsed(response, "user-master.html")
+        self.assertTemplateUsed(response, "account/signup.html")
+        form = response.context_data["form"]
+        self.assertEqual(len(form.fields), 6)
+        self.assertEqual(form.fields["username"].label, "Username")
+        self.assertEqual(form.fields["email"].label, "Email")
+        self.assertEqual(form.fields["email2"].label, "Email (again)")
+        self.assertEqual(form.fields["password1"].label, "Password")
+        self.assertEqual(form.fields["password2"].label, "Password (again)")
+        honeypot_attrs = form.fields["phone_number"].widget.attrs
+        self.assertEqual(honeypot_attrs["style"], "position: absolute; right: -99999px;")
+        self.assertEqual(honeypot_attrs["tabindex"], "-1")
+
+    def test_post_with_email_mismatch_does_not_create_account_or_send_email_but_reports_errors(self):
+        # Arrange
+        data = {
+            "username": "testuser",
+            "email": "testuser@example.com",
+            "email2": "testuser2@example.com",
+            "password1": "Yfr_A0Qdk7W-s2s01Mec ",
+            "password2": "Yfr_A0Qdk7W-s2s01Mec ",
+        }
+
+        # Act
+        response = self.client.post(self.signup_url, data)
+
+        # Assert
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "account/signup.html")
+        self.assertMessages(response, [])
+        form = response.context_data["form"]
+        self.assertEqual(len(form.errors), 1)
+        self.assertEqual(len(form.errors["email2"]), 1)
+        self.assertFormError(
+            form,
+            "email2",
+            "You must type the same email each time."
+        )
+        self.assertEqual(get_user_model().objects.count(), 0)
+        self.assertEqual(EmailAddress.objects.count(), 0)
+        self.assertEqual(len(mail.outbox), 0)
+
+    def test_post_with_password_mismatch_does_not_create_account_or_send_email_but_reports_errors(self):
+        # Arrange
+        data = {
+            "username": "testuser",
+            "email": "testuser@example.com",
+            "email2": "testuser@example.com",
+            "password1": "Yfr_A0Qdk7W-s2s01Mec1",
+            "password2": "Yfr_A0Qdk7W-s2s01Mec ",
+        }
+
+        # Act
+        response = self.client.post(self.signup_url, data)
+
+        # Assert
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "account/signup.html")
+        self.assertMessages(response, [])
+        form = response.context_data["form"]
+        self.assertEqual(len(form.errors), 1)
+        self.assertEqual(len(form.errors["password2"]), 1)
+        self.assertFormError(
+            form,
+            "password2",
+            "You must type the same password each time."
+        )
+        self.assertEqual(get_user_model().objects.count(), 0)
+        self.assertEqual(EmailAddress.objects.count(), 0)
+        self.assertEqual(len(mail.outbox), 0)
+
+    def test_post_with_similar_password_does_not_create_account_or_send_email_but_reports_errors(self):
+        # Arrange
+        data = {
+            "username": "testuser",
+            "email": "testuser@example.com",
+            "email2": "testuser@example.com",
+            "password1": "Test-user1",
+            "password2": "Test-user1",
+        }
+
+        # Act
+        response = self.client.post(self.signup_url, data)
+
+        # Assert
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "account/signup.html")
+        self.assertMessages(response, [])
+        form = response.context_data["form"]
+        self.assertEqual(len(form.errors), 1)
+        self.assertEqual(len(form.errors["password1"]), 1)
+        self.assertFormError(
+            form,
+            "password1",
+            "The password is too similar to the username."
+        )
+        self.assertEqual(get_user_model().objects.count(), 0)
+        self.assertEqual(EmailAddress.objects.count(), 0)
+        self.assertEqual(len(mail.outbox), 0)
+
+    def test_post_with_short_password_does_not_create_account_or_send_email_but_reports_errors(self):
+        # Arrange
+        data = {
+            "username": "testuser",
+            "email": "testuser@example.com",
+            "email2": "testuser@example.com",
+            "password1": "Yfr_A1",
+            "password2": "Yfr_A1",
+        }
+
+        # Act
+        response = self.client.post(self.signup_url, data)
+
+        # Assert
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "account/signup.html")
+        self.assertMessages(response, [])
+        form = response.context_data["form"]
+        self.assertEqual(len(form.errors), 1)
+        self.assertEqual(len(form.errors["password1"]), 1)
+        self.assertFormError(
+            form,
+            "password1",
+            "This password is too short. It must contain at least 8 characters."
+        )
+        self.assertEqual(get_user_model().objects.count(), 0)
+        self.assertEqual(EmailAddress.objects.count(), 0)
+        self.assertEqual(len(mail.outbox), 0)
+
+    def test_post_with_common_password_does_not_create_account_or_send_email_but_reports_errors(self):
+        """
+        This validator is not the best. It validates against django/contrib/auth/common-passwords.txt.gz,
+        which contains 19640 entries, but a quick inspection shows that a password such as "Password123!"
+        would pass simply because it has the added special character. We may add pwned-passwords-django
+        to the mix to combat this in the future.
+        """
+        # Arrange
+        data = {
+            "username": "testuser",
+            "email": "testuser@example.com",
+            "email2": "testuser@example.com",
+            "password1": "Pass_2011",
+            "password2": "Pass_2011",
+        }
+
+        # Act
+        response = self.client.post(self.signup_url, data)
+
+        # Assert
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "account/signup.html")
+        self.assertMessages(response, [])
+        form = response.context_data["form"]
+        self.assertEqual(len(form.errors), 1)
+        self.assertEqual(len(form.errors["password1"]), 1)
+        self.assertFormError(
+            form,
+            "password1",
+            "This password is too common."
+        )
+        self.assertEqual(get_user_model().objects.count(), 0)
+        self.assertEqual(EmailAddress.objects.count(), 0)
+        self.assertEqual(len(mail.outbox), 0)
+
+    def test_post_with_numeric_password_does_not_create_account_or_send_email_but_reports_errors(self):
+        """
+        This test will hit multiple errors: common, numeric, lacking letters, lacking lower, lacking special.
+        """
+        # Arrange
+        data = {
+            "username": "testuser",
+            "email": "testuser@example.com",
+            "email2": "testuser@example.com",
+            "password1": "31415926",
+            "password2": "31415926",
+        }
+
+        # Act
+        response = self.client.post(self.signup_url, data)
+
+        # Assert
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "account/signup.html")
+        self.assertMessages(response, [])
+        form = response.context_data["form"]
+        self.assertEqual(len(form.errors), 1)
+        self.assertEqual(len(form.errors["password1"]), 6)
+        self.assertFormError(
+            form,
+            "password1",
+            [
+                "This password is too common.",
+                "This password is entirely numeric.",
+                "This password must contain at least 4 letters.",
+                "This password must contain at least 1 upper case letter.",
+                "This password must contain at least 1 lower case letter.",
+                "This password must contain at least 1 special character."
+            ]
+        )
+        self.assertEqual(get_user_model().objects.count(), 0)
+        self.assertEqual(EmailAddress.objects.count(), 0)
+        self.assertEqual(len(mail.outbox), 0)
+
+    def test_post_with_valid_data_creates_account_and_sends_email(self):
+        # Arrange
+        data = {
+            "username": "testuser",
+            "email": "testuser@example.com",
+            "email2": "testuser@example.com",
+            "password1": "Yfr_A0Qdk7W-s2s01Mec ",
+            "password2": "Yfr_A0Qdk7W-s2s01Mec ",
+        }
+
+        # Act
+        response = self.client.post(self.signup_url, data)
+
+        # Assert
+        self.assertRedirects(response, self.redirect_url)
+        self.assertMessages(response, [Message(messages.INFO, f"Confirmation email sent to {data['email']}.")])
+        self.assertEqual(get_user_model().objects.count(), 1)
+        user = get_user_model().objects.get(username="testuser")
+        email_record = EmailAddress.objects.filter(user=user).first()
+        self.assertIsNotNone(email_record)
+        self.assertEqual(email_record.email, data["email"])
+        self.assertFalse(email_record.verified)
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].subject, "[testserver] Please Confirm Your Email Address")
+        self.assertEqual(mail.outbox[0].to, [user.email])
+
+
 class TestUnauthenticatedLoginView(MessagesTestMixin, TestCase):
     """
     This class tests our login view.
